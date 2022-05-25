ac59cc20fcc73bcdc0c73b573b94d2f00b1c3bc1
Ojochogwu Dickson <70067819+Ojochogwu866@users.noreply.github.com>
2022-05-25 22:59:57 +0100

    --dark-mode

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import DarkMode from './Darkmode';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DarkMode/>
+        <p>Hi, I am react Dark Mode, Nice to see you.
+         <br/>
+         Let's Have Fun
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
